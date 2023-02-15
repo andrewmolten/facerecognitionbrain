32654fd091c04d423512d82173b32408dc3c644e
andrewmolten <andrewmolten23@gmail.com>
2023-02-16 12:07:47 +1300

    Components added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 import "./App.css";
 import "tachyons";
+import ParticlesBg from "particles-bg";
+
+let config = {
+  color: ["random", "#ff0000"],
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <>
+          <div>...</div>
+          <ParticlesBg config={config} type="cobweb" bg={true} />
+        </>
         <Navigation />
-        {/* <Logo />
+        <Logo />
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/*<FaceRecognition /> */}
       </div>
     );
   }
